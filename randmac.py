c7cd6676163c155dbf40ef8d9d6a0253ca93fc42
dumplab <notsure700@gmx.ch>
2019-03-28 09:48:59 +0100

    Update randmac.py

--- randmac.py
+++ randmac.py
@@ -12,3 +12,10 @@ while True:
                 break
         cnt += 1
         sendp(Ether(src=RandMAC(),dst="FF:FF:FF:FF:FF:FF")/ARP(op=2, psrc="0.0.0.0", hwdst="FF:FF:FF:FF:FF:FF")/Padding(load="X"*18),iface=interface)
+
+        
+        
+        # or an ip/udp packet
+#        randSrcIP = "172.19.19." + str(cnt+3)
+#        print("Random IP: " + randSrcIP)
+#        sendp(Ether(src=RandMAC(), dst="ff:ff:ff:ff:ff:ff")/IP(src=randSrcIP,dst="172.20.20.20")/UDP(dport=9)/b"hey",iface=interface)
